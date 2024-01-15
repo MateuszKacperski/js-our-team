3f26967b14f55cd7a4f9edf7ea5e6f83cb301900
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-15 14:43:08 +0100

    print in console employers information

--- js/script.js
+++ js/script.js
@@ -1,37 +1,47 @@
 console.log('JS ok');
 
-
+// creo arry di oggetti ossia gli impiegati
 const employers = [
     {
-        name: 'Wayne Barnett',
-        role: 'Founder & CEO ',
-        img: 'wayne-barnett-founder-ceo.jpg'
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO ',
+        image : 'wayne-barnett-founder-ceo.jpg'
     },
     {
-        name: 'Angela Caroll',
-        role: 'Chief Editor',
-        img: 'angela-caroll-chief-editor.jpg'
+        name : 'Angela Caroll',
+        role : 'Chief Editor',
+        image : 'angela-caroll-chief-editor.jpg'
     },
     {
-        name: 'Walter Gordon',
-        role: 'Office Manager',
-        img: 'walter-gordon-office-manager.jpg'
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        image : 'walter-gordon-office-manager.jpg'
     },
     {
-        name: 'Angela Lopez',
-        role: 'Social Media Manager',
-        img: 'angela-lopez-social-media-manager.jpg'
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
+        iamge : 'angela-lopez-social-media-manager.jpg'
     },
     {
-        name: 'Scott Estrada',
-        role: 'Developer',
-        img: 'scott-estrada-developer.jpg'
+        name : 'Scott Estrada',
+        role : 'Developer',
+        image : 'scott-estrada-developer.jpg'
     },
     {
-        name: 'Barbara Ramos',
-        role: 'Graphic Designer',
-        img: 'barbara-ramos-graphic-designer.jpg'
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        image : 'barbara-ramos-graphic-designer.jpg'
     }
 ]
 
-console.log(employers);
+//Ciclo per stampare i dati dei employers
+
+for(let i = 0; i < employers.length;i++){
+    const nome = employers[i].name;
+    const ruolo = employers[i].role;
+    const image = employers[i].image;
+    console.log(`nome : ${nome}`);
+    console.log(`ruolo : ${ruolo}`);
+    console.log(`immagine : ${image}`);
+  
+}
